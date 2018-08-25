87618133c608b5dca4eb065ff0dccc3e27df7596
udhayapriyaalamelu <40052766+udhayapriyaalamelu@users.noreply.github.com>
2018-08-25 12:48:44 +0530

    Add files via upload

---
+++ Program12.java
@@ -0,0 +1,33 @@
+package guvi;
+import java.util.*;
+public class Program12 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int arr[]=new int[n];
+		for(int i=0;i<n;i++)
+		{
+			arr[i]=sc.nextInt();
+		}
+		System.out.println("enter the position to search:");
+		int pos=sc.nextInt();
+		int max=0;
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<n;j++)
+			{
+				if(arr[i]>arr[j])
+					
+				{
+					max=arr[j];
+					arr[j]=arr[i];
+					arr[i]=max;
+				}
+			}
+		}
+			System.out.println(arr[n-pos]);
+
+			}
+	}
